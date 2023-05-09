24cb9ed339d0f0d8c68e226f1f424932428e1d95
Tyagi1337 <125657365+Tyagi1337@users.noreply.github.com>
2023-05-09 23:59:37 +0530

    Updated

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -14,6 +14,7 @@ class CustomUser(AbstractUser):
     user_type_data=((1,"HOD"),(2,"Staff"),(3,"Student"))
     user_type=models.CharField(default=1,choices=user_type_data,max_length=10)
 
+
 class AdminHOD(models.Model):
     id=models.AutoField(primary_key=True)
     admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
